6bec41437404dc0d568696c654cb4b250c87d9fb
ianmungai <44510735+ianmungai@users.noreply.github.com>
2020-02-17 14:34:34 -0700

    Update fsm.py

--- fsm.py
+++ fsm.py
@@ -165,8 +165,8 @@ def Handler(leftRight, upDown, speedfactor):
 	for event in events:
 		if event.type == pygame.QUIT:
             # User exit
-            hadEvent = True
-            moveQuit = True
+           hadEvent = True
+           moveQuit = True
         elif event.type == pygame.JOYAXISMOTION:
             # A joystick has been moved, read axis positions (-1 to +1)
             hadEvent = True
